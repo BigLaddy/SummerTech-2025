a7a2fd8af7be34cb02f7104a4e90f2470703aa74
BigLaddy <wujx401@gmail.com>
2025-07-14 13:39:34 +0000

    Day 2

--- Number1.py
+++ Number1.py
@@ -3,7 +3,6 @@ Player2Left=1
 Player1Right=1
 Player2Right=1
 
-
 def Combine(x,y):
     Total = x + y
     Left = Total
@@ -29,8 +28,16 @@ def Combine(x,y):
                     return [x,y]
     Combine(x,y)
 
+def Attack(PLeft,PRight,ELeft,ERight,Turn):
+    if Turn == 2:
+        x=ELeft
+        ELeft = PLeft
+        PLeft=x 
+        y=ERight
+        ERight = PRight
+        PRight=y 
 
-def Attack(PLeft,PRight,ELeft,ERight):
+
     UsedHand=input("Would you like to use your left or right hand?")
     if UsedHand == "Left":
         AttackValue = PLeft
@@ -41,19 +48,22 @@ def Attack(PLeft,PRight,ELeft,ERight):
         ELeft += AttackValue
         if ELeft > 4:
             ELeft = 0
+        if Turn == 2:
+            return [ELeft,ERight,PLeft,PRight]
         return[PLeft,PRight,ELeft,ERight]
     else:
         ERight += AttackValue
         if ERight > 4:
             ERight = 0
+        if Turn == 2:
+            return [ELeft,ERight,PLeft,PRight]
         return[PLeft,PRight,ELeft,ERight]
     
-
 def Choice(P1L,P1R,P2L,P2R,Turn):
      if Turn == 1:
         Choice = input("Would you like to attack or combine?" )
         if Choice == "Attack":
-            return Attack(P1L,P1R,P2L,P2R)
+            return Attack(P1L,P1R,P2L,P2R,1)
         if Choice == "Combine":
             List = Combine(P1L,P1R)
             List.append(P2L)
@@ -62,144 +72,31 @@ def Choice(P1L,P1R,P2L,P2R,Turn):
      if Turn == 2:
         Choice = input("Would you like to attack or combine?" )
         if Choice == "Attack":
-            return Attack(P1L,P1R,P2L,P2R)
+            return Attack(P1L,P1R,P2L,P2R,2)
         if Choice == "Combine":
-            List = Combine(P1L,P1R)
-            List.append(P2L)
-            List.append(P2R)
+            List = Combine(P2L,P2R)
+            List.insert(0,P1L)
+            List.insert(1,P1R)
             return List
-          
-
-
-print(Choice(Player1Left,Player1Right,Player2Left,Player2Left))
-
-
         
-        
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+def CheckWin(P1L,P1R,P2L,P2R):
+    if P1L == 0 and P1R == 0:
+        return "P2Win"
+    elif P2L == 0 and P2R == 0:
+        return "P1Win"
+    else:
+        return "NoWin"
+
+PlayerValues = (Choice(Player1Left,Player1Right,Player2Left,Player2Right,1))
+print(PlayerValues)
+while CheckWin(PlayerValues[0],PlayerValues[1],PlayerValues[2],PlayerValues[3]) == "NoWin":
+    PlayerValues = (Choice(PlayerValues[0],PlayerValues[1],PlayerValues[2],PlayerValues[3],2))
+    print(PlayerValues)
+    if CheckWin(PlayerValues[0],PlayerValues[1],PlayerValues[2],PlayerValues[3]) !="NoWin":
+        print(CheckWin(PlayerValues[0],PlayerValues[1],PlayerValues[2],PlayerValues[3]))
+        continue
+    PlayerValues = (Choice(PlayerValues[0],PlayerValues[1],PlayerValues[2],PlayerValues[3],1))
+    print(PlayerValues)
+    if CheckWin(PlayerValues[0],PlayerValues[1],PlayerValues[2],PlayerValues[3]) !="NoWin":
+        print(CheckWin(PlayerValues[0],PlayerValues[1],PlayerValues[2],PlayerValues[3]))
+        continue
