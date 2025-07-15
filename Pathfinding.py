a7081f3ec8fea4dc8475726e85ea361a4bfb2d73
BigLaddy <wujx401@gmail.com>
2025-07-15 14:30:52 +0000

    Day 3

--- Pathfinding.py
+++ Pathfinding.py
@@ -1,27 +1,63 @@
 class node():
-    def __init__(self,edges):
+    def __init__(self,edges,name):
         self.edges = edges
-
+        self.name = name
+    def __str__(self):
+        print(self.name)
 class graph():
     def __init__(self):
         self.nodes = []
     def Node(self,AddedNode):
         self.nodes.append(AddedNode)           
     def Edge(self,Node1,Node2):
-        Node1.edges.append(Node2)
-        Node2.edges.append(Node1)   
+        self.nodes[Node1].edges.append(self.nodes[Node2])
+        self.nodes[Node2].edges.append(self.nodes[Node1])   
     def HasEdge(self,Node1,Node2):
-        for x in range(0,len(Node1.edges)):
-            if Node1.edges[x]==Node2:
+        for x in range(0,len(self.nodes[Node1].edges)):
+            if self.nodes[Node1].edges[x]==self.nodes[Node2]:
                 return True
         return False
+class pathfinder():
+    def __init__(self,Graph,Start,End,Current,Traveled):
+        self.Graph = Graph
+        self.Start = Start
+        self.End = End
+        self.Current= Current
+        self.Traveled = Traveled
+    def FindPath(self):
+        if self.Current == self.End:
+            print(self.Traveled)
+            return self.Traveled.append(self.Current)
+        
+        for x in range (0,len(self.Current.edges)):
+            print("In for loop")
+            Con = True
+            for y in range(0,len(self.Traveled)):
+                print(y)
+                
+                if self.Traveled[y] == self.Current.edges[x]:
+                    Con = False
+            if Con:
+                self.Traveled.append(self.Current)
+                print(len(self.Traveled))
+                pathfinder(Graph,self.Start,self.End,self.Current.edges[x],self.Traveled).FindPath()
+    
+
 
 Graph = graph()
-Graph.Node(node([]))
-Graph.Node(node([]))
-Graph.Node(node([]))
-Graph.Edge(Graph.nodes[0],Graph.nodes[1])
-print(Graph.HasEdge(Graph.nodes[0],Graph.nodes[1]))
-print(Graph.HasEdge(Graph.nodes[0],Graph.nodes[2]))
+Graph.Node(node([],1))
+Graph.Node(node([],2))
+Graph.Node(node([],3))
+Graph.Node(node([],4))
+Graph.Node(node([],5))
+Graph.Node(node([],6))
+Graph.Edge(0,1)
+Graph.Edge(1,2)
+Graph.Edge(1,3)
+Graph.Edge(3,4)
+Graph.Edge(4,5)
+Finder = pathfinder(Graph,Graph.nodes[0],Graph.nodes[5],Graph.nodes[0],[Graph.nodes[0]])
+print(Finder.FindPath())
+
 
     
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,11 +1,2 @@
-print(Left,Right)
-            Answer=input("Would you like this combination?")
-            if Answer == "Yes":
-                ReturnableList=[Left,Right]
-                return ReturnableList
-            else:
-                Left-=1
-                Right+=1
-                if Right > 4:
-                    print("There is no longer a valid choice")
-                    return [x,y]
+    self.Traveled.append(self.Current)
+                pathfinder(Graph,self.Start,self.End,self.Current.edges[x],self.Traveled).FindPath()
