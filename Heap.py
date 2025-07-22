a37875b6063a8b5a5db680bbedd702ddc4bed748
BigLaddy <wujx401@gmail.com>
2025-07-22 14:45:01 +0000

    day 9

--- Heap.py
+++ Heap.py
@@ -6,13 +6,15 @@ def getParent(index):
     if index == 0:
         return None
     if index%2 == 1:
-        return (index-2)/2
+        return int((index-2)/2)
     else:
-        return (index-1)/2
+        return int((index-1)/2)
 class Node():
-    def __init__(self,left,right,parent,sizeleft,sizeright,priority,value):
+    def __init__(self,priority,value):
         self.priority = priority
         self.value = value
+    def __repr__(self):
+        return str(self.priority)
 
 
 class badHeap():
@@ -34,15 +36,65 @@ class Heap():
         self.size = len(self.Heap)
         self.Heap.append(newNode)
         limit = False
+        if self.size == 0:
+            return
         index = self.size
-        while limit != True:
-            if self.Heap[getParent(index)].priority > priority:
-                temp = self.Heap[getParent(index)]
-                self.Heap[getParent(index)] = newNode
+        while limit == False:
+            x = getParent(index)
+            if x is None:
+                limit = True
+                continue
+            if self.Heap[x].priority < priority:
+                temp = self.Heap[x]
+                self.Heap[x] = newNode
                 self.Heap[index]= temp
-                index = getParent(index)
+                index = x
                 continue
             limit = True 
+    def remove(self):
+        y = self.Heap[0]
+        self.Heap[0] = self.Heap[len(self.Heap)-1]
+        self.Heap[len(self.Heap)-1] = y
+        value = self.Heap.pop(len(self.Heap)-1)
+        limit = False
+        x = 0
+        while limit != True:
+            if getRightChild(x) >len(self.Heap)-1 and getLeftChild(x) >len(self.Heap)-1:
+                limit = True
+                continue
+            if getLeftChild(x) >len(self.Heap)-1:
+                limit = True
+                continue
+            if self.Heap[getLeftChild(x)].priority > self.Heap[x].priority:
+                if getRightChild(x) <= len(self.Heap)-1 and self.Heap[getRightChild(x)].priority > self.Heap[getLeftChild(x)].priority:
+                    temp = self.Heap[getRightChild(x)]
+                    self.Heap[getRightChild(x)] = self.Heap[x]
+                    self.Heap[x]= temp
+                    x = getRightChild(x)
+                    continue
+                temp = self.Heap[getLeftChild(x)]
+                self.Heap[getLeftChild(x)] = self.Heap[x]
+                self.Heap[x]= temp
+                x = getLeftChild(x)
+                continue
+            if getRightChild(x) >len(self.Heap)-1:
+                limit = True
+                continue
+            if self.Heap[getRightChild(x)].priority > self.Heap[x].priority:
+                temp = self.Heap[getRightChild(x)]
+                self.Heap[getRightChild(x)] = self.Heap[x]
+                self.Heap[x]= temp
+                x = getRightChild(x)
+                continue
+            limit = True
+        return value
+heap = Heap()
+for x in range(0,10):
+    heap.add(x,x)
+for x in range(0,10):
+    print(heap.remove())
+
+
 
 
 
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,4 +1,2 @@
-for x in range(-1,len(self.PQueue)-1):
-                if element.priority>self.PQueue[x+1].priority:
-                    self.PQueue.insert(x+1,element)
-                    return
+    limit = True
+                    continue
